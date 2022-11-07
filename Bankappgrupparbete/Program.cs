1dbdb83b298b9c0fe6d35e8bbec62f453ac23095
User <User@USER-PC>
2022-11-07 14:19:19 +0100

    Add project files.

---
+++ Bankappgrupparbete/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Bankappgrupparbete
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
