2f45c976ca921f23781a7af3cc26e33c03534f8e
KORNANDY666 <113092594+KORNANDY666@users.noreply.github.com>
2022-11-09 19:15:02 +0100

    jaja igen / ANdreas

--- Bankappgrupparbete/Admin.cs
+++ Bankappgrupparbete/Admin.cs
@@ -10,63 +10,9 @@ namespace Bankappgrupparbete
     {
         public Admin(string username, string password) : base(username, password)
         {
-        }
-
-        public static void adminInlogg()
-        {
-            bool gameOver = false;
-            int num = 0;
-            while (gameOver == false)
-            {
-                Console.Write("Användarnamn : ");
-                string adminUser = Console.ReadLine().ToLower();
-                Console.Write("Password : ");
-                string adminPass = Console.ReadLine().ToLower();
-                num++;
-                if (adminUser == "admin" && adminPass == "admin")
-                {
-                    Console.Clear();
-                    Console.WriteLine("Välkommen till inloggningen för Admin !!");
-                    Console.WriteLine("1. Lägga till användare");
-                    Console.WriteLine("2. Kolla något annat");
-                    int adminInput = int.Parse(Console.ReadLine());
-                    if (adminInput == 1)
-                    {
-                        addUser();
-                    }
-                    gameOver = true;
-                    Console.ReadKey();
-                }
-                else
-                {
-                    if (num < 3)
-                    {
-                        Console.WriteLine("Fel inmatning, försök igen!");
-                        gameOver = false;
-                        Console.Clear();
-                    }
-                    else if (num == 3)
-                    {
-                        Console.Clear();
-                        Console.WriteLine("Nu har du ett försök kvar!!");
-                    }
-                    else
-                    {
-                        Console.Clear();
-                        Console.WriteLine("Va fan, jag sa ju till dig!!. Nu loggas du ut!");
-                        break;
-                    }
-                }
-            }
-        }
-        public static void addUser()
-        {
-            Console.WriteLine("Ange info om den nya användaren");
-            Console.WriteLine("-------------------------------");
-            Console.Write("Användarnamn : ");
-
-
 
         }
+
+        
     }
 }
--- Bankappgrupparbete/Bank.cs
+++ Bankappgrupparbete/Bank.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Net.WebSockets;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading;
+
 
 namespace Bankappgrupparbete
 {
@@ -11,11 +13,13 @@ namespace Bankappgrupparbete
     {
         string Username;
         string Password;
+        
 
         public string username { get { return Username; } set { Username = value; } }
         public string password { get { return Password; } set { Password = value; } }
 
-        List<Customer> Clist = new List<Customer>();
+        List<string> userList = new List<string>();
+        List<string> passList = new List<string>();
 
         public Bank(string username, string password)
         {
@@ -34,15 +38,111 @@ namespace Bankappgrupparbete
             switch (input)
             {
                 case 1:
-                    Admin.adminInlogg();
+                    adminInlogged();
                     break;
                 default:
                     break;
             }
         }
+        public void adminInlogged()
+        {
+            bool gameOver = false;
+            int num = 0;
+            while (gameOver == false)
+            {
+                Console.Write("Användarnamn Admin : ");
+                string adminUser = Console.ReadLine().ToLower();
+                Console.Write("Password Admin : ");
+                string adminPass = Console.ReadLine().ToLower();
+                num++;
+                if (adminUser == "admin" && adminPass == "admin")  {admin();}
+                else
+                {
+                    if (num < 3)
+                    {
+                        Console.WriteLine("Fel inmatning, försök igen!");
+                        gameOver = false;
+                        Console.Clear();
+                    }
+                    else if (num == 3)
+                    {
+                        Console.Clear();
+                        Console.WriteLine("Nu har du ett försök kvar!!");
+                    }
+                    else
+                    {
+                        Console.Clear();
+                        Console.WriteLine("Va fan, jag sa ju till dig!!. Nu loggas du ut!");
+                        break;
+                    }
+                }
+            }
+
+        }
+        public void admin()
+        {
+            Console.Clear();
+            Console.WriteLine("Välkommen till inloggningen för Admin !!");
+            Console.WriteLine("1. Lägga till användare");
+            Console.WriteLine("2 Användare ");
+            Console.WriteLine("3. Logga ut");
+            int adminInput = int.Parse(Console.ReadLine());
+            if (adminInput == 1) { addUser(); }
+            else if (adminInput == 2) { checkUser(); }
+            else if (adminInput == 3) { Console.Clear(); Start();  }
+            Console.ReadKey();
+        }
+        public void addUser()
+        {
+            int num = 1;
+            Console.Clear();
+            Console.Write("Hur många vill du lägga till : ");
+            int total = int.Parse(Console.ReadLine());
+            Console.WriteLine("-------------------------------");
+            
+            for (int i = 0; i < total; i++)
+            {
+                Console.Write($"Användarnamn {i + 1} : ");
+                var input = Console.ReadLine();
+                if (input.Length < 5)
+                {
+                    
+                    Console.WriteLine("Minst 5 tecken TACK");
+                    Thread.Sleep(5000);
+                    addUser();
+                }
+                userList.Add(input);
+            }
+            Console.WriteLine("-----------------------------------------");
+            for (int i = 0; i < total; i++)
+            {
+                Console.Write($"PassWord {i+1} : ");
+                var input = Console.ReadLine();
+                if (input.Length < 5)
+                {
+                    Console.WriteLine("Minst 5 tecken TACK");
+                    addUser();
+                }
+                passList.Add(input);
+            }
+            Console.Clear();
+            admin();
+        }
+        public void checkUser()
+        {
+            Console.WriteLine("Här kommer alla användare");
+            foreach (var item in userList)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("------------------");
+            Console.WriteLine(" Tryck Enter för att återvända till start");
+            Console.ReadKey(); 
+            admin();
+        }
 
 
-        
+
 
 
 
