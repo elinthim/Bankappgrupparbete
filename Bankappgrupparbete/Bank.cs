3b5e143d0be71a50f2102d4f4f06130ac0758c30
Elin Thim <elin_thim@hotmail.com>
2022-11-14 14:08:20 +0100

    banksaldo

--- Bankappgrupparbete/Bank.cs
+++ Bankappgrupparbete/Bank.cs
@@ -10,12 +10,14 @@ namespace Bankappgrupparbete
         string Username;
         string Password;
 
+        //Admin a = new Admin("",""); 
 
         public string username { get { return Username; } set { Username = value; } }
         public string password { get { return Password; } set { Password = value; } }
 
         List<string> userList = new List<string>();
         List<string> passList = new List<string>();
+        List<string> banklist = new List<string>();
         DateTime currentDateTime = DateTime.Now;
 
         public Bank(string username, string password)
@@ -109,8 +111,8 @@ namespace Bankappgrupparbete
 
                 if (userfound == customerinlog && userfound1 == customerPass)
                 {
-                    Console.WriteLine("Användarnamn godkänt du skickas vidare till nästa menyval");
-                    gameOver = true;
+                    usermeny();
+
                 }
                 else if (userfound != customerinlog && userfound1 != customerPass)
                 {
@@ -119,7 +121,11 @@ namespace Bankappgrupparbete
                     Console.Clear();
 
                 }
-                if (num == 3)
+                if (num < 3)
+                {
+                    Console.WriteLine("Fel inmatning försök igen");
+                }
+                else if (num == 3)
                 {
                     Console.Clear();
                     Console.WriteLine("Nu har du ett försök kvar!!");
@@ -208,6 +214,47 @@ namespace Bankappgrupparbete
             Console.WriteLine("Tryck Enter för att återvända till start");
             Console.ReadKey();
             admin();
+        }
+        public void usermeny()
+        {
+            Console.Clear();
+            Console.WriteLine("Välkommen till din användarmenyn");
+            Console.WriteLine("1. Se mina bankkonton");
+            Console.WriteLine("2. Öppna nytt bankkonto ");
+            Console.WriteLine("3. Logga ut");
+            int adminInput = int.Parse(Console.ReadLine());
+            if (adminInput == 1) { banksaldo(); }
+            else if (adminInput == 2) { checkUser(); }
+            else if (adminInput == 3) { Console.Clear(); Start(); }
+            Console.ReadKey();
+
+                 switch (adminInput)
+            {
+                case 1:
+                    usermeny();
+                    break;
+                case 2:
+                    
+                    break;
+                case 3:
+                    Console.Clear();
+                    Console.WriteLine("Tack för idag");
+                    Console.ReadKey();
+                    break;
+                default:
+                    break;
+            }
+
+        }
+        public void banksaldo()
+        {
+            Random random = new Random();
+            int number1 = random.Next(1111111,9999999);
+            Random random1 = new Random();
+            int number2 = random.Next(0, 1000000);
+
+            Console.WriteLine("Kontonummer : " + number1);
+            Console.WriteLine("Banksaldo : " + number2  + " kr");
         }
     }
 }
